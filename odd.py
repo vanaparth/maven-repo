c1c5d72e3ddd496647620c220463d5e527f648c3
Numeric <vanaparthi.rajesh@gmai.com>
2016-05-03 10:43:25 -0700

    editted the file

--- odd.py
+++ odd.py
@@ -1,6 +1,6 @@
 num1 = int(raw_input("enter a number1"))
 num2 = int(raw_input("enter a number2"))
-
+num3 = int(raw_input("enter a number2")
 if num1==num2:
   print "num1 and num2 are equal"
 elif num1>num2:
