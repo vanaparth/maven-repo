6767f55d83473e3347eb9e519de776536d891f2b
Numeric <vanaparthi.rajesh@gmai.com>
2016-05-11 13:52:21 -0700

    Squashed 'target2/' content from commit afa0414
    git-subtree-dir: target2
    git-subtree-split: afa04145e0d9c7fbd02a20b9ac2acb800a028622

---
+++ randoms.py
@@ -0,0 +1,11 @@
+import random
+heighest=10
+answer=random.randrange(heighest)
+guess=raw_input("enter the number from 0 to %d"%heighest)
+while (int(guess)!= answer):
+   if(int(guess)<answer):
+     print ("the number is heigher")
+   else:
+     print ("the number is lower")
+     guess = raw_input("enter the number from 0 to %d",heighest)
+raw_input("you are at winner face")
