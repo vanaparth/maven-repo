b9bb58f7eb3bc3e110942ae3f3f84d7deff0a723
Numeric <vanaparthi.rajesh@gmai.com>
2016-05-23 16:23:50 -0700

    changed the changes

--- random.py
+++ random.py
@@ -10,4 +10,13 @@ while (int(guess)!= answer):
      print ("the number is lower")
      guess = raw_input("enter the number from 0 to %d",heighest)
 raw_input("you are at winner face")
+
+while (int(guess)!= answer):
+   if(int(guess)<answer):
+     print ("the number is heigher")
+   else:
+     print ("the number is lower")
+     guess = raw_input("enter the number from 0 to %d",heighest)
+raw_input("you are at winner face")
+r
 r
